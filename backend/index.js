6f5bf27947b5db414b1b5488bcb61701aa09613d
Jaishikha-Sharma <jaishikhasharma05@gmail.com>
2025-05-21 11:30:22 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,4 @@
-// import express, { json } from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import userRoute from "./routes/user.route.js";
-// import dotenv from "dotenv";
-// import connectDB from "./utils/db.js";
-// import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
-// const app = express();
 
-// dotenv.config({});
-
-// //middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// const corsOptions = {
-//   origin: ["http://localhost:5173", "https://frontend-91qr590kg-jaishikha-sharmas-projects.vercel.app" ,  "https://frontend-rho-orcin-27.vercel.app",] ,
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   optionsSuccessStatus: 200,
-// };
-
-// app.use(cors(corsOptions));
-
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server running at port ${PORT}`);
-// });
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
