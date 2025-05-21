5e9a885588c313feba8b214c2c297fb7d270cfba
Jaishikha-Sharma <jaishikhasharma05@gmail.com>
2025-05-21 16:48:46 +0530

    risky code h

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,10 @@
-const isProduction = window.location.hostname !== "localhost";
-const BASE_URL = isProduction
-  ? "https://job-portal-backend-2tyj.onrender.com"
-  : "http://localhost:8000";
+const BASE_URL =
+  import.meta.env.VITE_BASE_URL ||
+  (window.location.hostname === "localhost" ? "http://localhost:8000" : "https://job-portal-backend-2tyj.onrender.com");
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
 export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+
+console.log("BASE_URL is:", BASE_URL);
