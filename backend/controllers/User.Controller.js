83c7ba2de3513361599a5440184a7856a758c3eb
Jaishikha-Sharma <jaishikhasharma05@gmail.com>
2025-05-22 10:56:59 +0530

    22 may code

--- backend/controllers/User.Controller.js
+++ backend/controllers/User.Controller.js
@@ -108,8 +108,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        secure: true,
+        sameSite: "None",
       })
       .json({
         message: `Welcome Back ${user.fullname}`,
